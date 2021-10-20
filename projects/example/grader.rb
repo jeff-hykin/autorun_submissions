bbbd0c2bf3f43dc5fb431fa84e8f0003e3746638
Jeff Hykin <jeff.hykin@gmail.com>
2021-10-20 17:33:01 -0500

    add example

---
+++ projects/example/grader.rb
@@ -0,0 +1,44 @@
+def grade_assignment(possible_submission_folders, submission_name)
+    # (ideally `possible_submission_folders` would only have one iteam but sometimes there are false positives)
+    results = []
+    for each_folder in possible_submission_folders
+        # skip the __pycache__ and __MACOSX folders
+        if each_folder =~ /\/(__pycache__|__MACOSX)\//
+            next
+        end
+        
+        # get the student's name & UIN
+        name_and_uin = submission_name.sub(/(\w+)_(\d+)_(\d\d\d\d\d\d\d\d).+/,"\\1,\\3")
+        name, uin = name_and_uin.split(",")
+        
+        # 
+        # run the command
+        # 
+        FileSystem.in_dir(each_folder) do
+            command_result = Console.run!(["python", each_folder/"autograder.py"])
+        end
+        
+        # 
+        # extract score
+        # 
+        output = command_result.text
+        total = output.gsub(/[\w\W]*(Total: .+)[\w\W]*\z/, "\\1")
+        if total and total =~ /Total/
+            total = total.gsub(/Total: /,"")
+        else
+            total = nil
+        end
+        
+        # 
+        # save data
+        # 
+        results.push({
+            "name" => name,
+            "uin" => uin,
+            "grade" => total,
+            "folder" => each_folder,
+            "autograder_output" => output,
+        })
+    end
+    return results
+end
